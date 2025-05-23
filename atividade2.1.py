63587eebe261a9ae4b2c9aa5c70cf1bd3736fa04
Anthony <anthony.matheusfrz@gmail.com>
2025-05-23 15:59:02 -0300

    Add files via upload

---
+++ atividade2.1.py
@@ -0,0 +1,6 @@
+while True:
+    n1= int(input("digite seu number: "))
+    if n1 % 2 == 0:
+        print("par")
+    else:
+        print("impar")
---
+++ atividade2.2.py
@@ -0,0 +1,6 @@
+while True:
+    n1= int(input("digite o ano: "))
+    if n1 % 4 == 0:
+        print("bissexto")
+    else:
+        print("not bissexto")
